e4bfb6691810e3d1a3ff2cb8eb45c568e33345ad
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-03 11:44:23 -0400

    syntax ex

---
+++ syntaxExercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace syntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+
+            //inferred typing
+            //var answer = 4;
+            //if (answer < 9)
+            //{
+            //    Console.WriteLine("answer" + " is less than nine.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("answer" + " is greater than or equal to nine");
+            //}
+
+
+            //string interpolation
+            //var answer = 4;
+
+            //if (answer < 9)
+            //{
+            //    Console.WriteLine($"{answer} is less than nine.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{answer} is greater than nine");
+            //}
+
+
+            //ternary
+            var answer = 4;
+            Console.WriteLine((answer < 9) ? $"{answer} is less than nine." : $"answer is greater than nine.");
+        }
+    }
+}
+
